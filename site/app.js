347b27b18991c1424a2e9a03ab5ade11d4e99673
Matt Frawley <docfrawley@gmail.com>
2016-09-25 15:54:47 -0400

    final code for assignment

---
+++ site/app.js
@@ -0,0 +1,77 @@
+(function (){
+
+'use strict';
+
+var initialitems = [
+  {
+    name: "oranges",
+    quantity: "5"
+  },
+  {
+    name: "pears",
+    quantity: "7"
+  },
+  {
+    name: "spinach",
+    quantity: "3"
+  },
+  {
+    name: "broccoli",
+    quantity: "4"
+  },
+  {
+    name: "kale",
+    quantity: "9"
+  }
+];
+
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyShoppingController', ToBuyShoppingController)
+  .controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var toBuy = this;
+
+    toBuy.items= ShoppingListCheckOffService.getItems();
+
+
+    toBuy.switchItem = function (itemIdex, item) {
+      ShoppingListCheckOffService.switchItem(itemIdex, item);
+    }
+  };
+
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var showList = this;
+
+    showList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+  };
+
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    // initial list of shopping items
+
+    var items = initialitems;
+    var boughtItems = [];
+
+    service.switchItem = function (itemIdex, item) {
+      items.splice(itemIdex, 1);
+      boughtItems.push(item);
+    };
+
+    service.getItems = function () {
+      return items;
+    };
+
+    service.getBoughtItems = function () {
+      return boughtItems;
+    };
+  }
+
+  })();
